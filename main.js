b86e7d62e1a635b165c30925429309cf88d0dcde
hendmissonen <hendmissonen@gmail.com>
2024-03-05 09:32:30 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,80 @@
+var form = document.querySelector("form");
+var customer = document.querySelector("#name");
+var age = document.querySelectorAll("[name=\"age\"]");
+var healthStatus = document.querySelectorAll("[name=\"status\"]");
+var googHabits = document.querySelectorAll("[name=\"goodHabit\"]");
+var badHabits = document.querySelectorAll("[name=\"badHabit\"]");
+var riskscore = document.querySelector("#riskscore");
+var riskscoreCal = function () {
+    var customerName = customer.value;
+    var ageResult = "";
+    var healthStatusResult = [];
+    var goodHabitResult = [];
+    var badHabitResult = [];
+    var score = "500";
+    //calculating riskscore for age
+    age.forEach(function (item) {
+        if (item.checked) {
+            ageResult = item.id;
+        }
+    });
+    switch (ageResult) {
+        case "age1":
+            score;
+            break;
+        case "age2":
+            score *= 1.1;
+            break;
+        case "age3":
+            score *= 1.3;
+            break;
+        case "age4":
+            score *= 1.6;
+            break;
+        case "age5":
+            score *= 2;
+            break;
+        case "age6":
+            score *= 2.5;
+            break;
+        case "age7":
+            score *= 3.1;
+            break;
+    }
+    //calculating riskscore for health status
+    healthStatus.forEach(function (disease) {
+        if (disease.checked) {
+            healthStatusResult.push(disease.value);
+        }
+    });
+    if (healthStatusResult.length > 0) {
+        score *= 1 + healthStatusResult.length / 100;
+    }
+    //calculating riskscore for good habits
+    googHabits.forEach(function (goodHabit) {
+        if (goodHabit.checked) {
+            goodHabitResult.push(goodHabit.value);
+        }
+    });
+    if (goodHabitResult.length > 0) {
+        score *= 1 - (goodHabitResult.length * 5) / 100;
+    }
+    //calculating riskscore for bad habits
+    badHabits.forEach(function (badHabit) {
+        if (badHabit.checked) {
+            badHabitResult.push(badHabit.value);
+        }
+    });
+    if (badHabitResult.length > 0) {
+        score *= 1 + (badHabitResult.length * 5) / 100;
+    }
+    //let #riskscore show score
+    riskscore.textContent = score;
+    if (typeof score === "number") {
+        riskscore.textContent = score.toFixed(2); // No error
+    }
+    else {
+        riskscore.textContent = score; // Handle the case where score is a string
+    }
+};
+form.addEventListener("input", riskscoreCal);
---
+++ main.ts
@@ -0,0 +1,96 @@
+const form = document.querySelector("form") as HTMLFormElement;
+const customer = document.querySelector("#name") as HTMLInputElement;
+const age = document.querySelectorAll(
+  `[name="age"]`
+) as NodeListOf<HTMLInputElement>;
+const healthStatus = document.querySelectorAll(
+  `[name="status"]`
+) as NodeListOf<HTMLInputElement>;
+const googHabits = document.querySelectorAll(
+  `[name="goodHabit"]`
+) as NodeListOf<HTMLInputElement>;
+const badHabits = document.querySelectorAll(
+  `[name="badHabit"]`
+) as NodeListOf<HTMLInputElement>;
+
+const riskscore = document.querySelector("#riskscore") as HTMLInputElement;
+
+const riskscoreCal = () => {
+  let customerName: string = customer.value;
+  let ageResult: string = "";
+  let healthStatusResult: string[] = [];
+  let goodHabitResult: string[] = [];
+  let badHabitResult: string[] = [];
+  let score: number | string = "500";
+
+  //calculating riskscore for age
+  age.forEach((item) => {
+    if (item.checked) {
+      ageResult = item.id;
+    }
+  });
+
+  switch (ageResult) {
+    case "age1":
+      score;
+      break;
+    case "age2":
+      score *= 1.1;
+      break;
+    case "age3":
+      score *= 1.3;
+      break;
+    case "age4":
+      score *= 1.6;
+      break;
+    case "age5":
+      score *= 2;
+      break;
+    case "age6":
+      score *= 2.5;
+      break;
+    case "age7":
+      score *= 3.1;
+      break;
+  }
+
+  //calculating riskscore for health status
+  healthStatus.forEach((disease) => {
+    if (disease.checked) {
+      healthStatusResult.push(disease.value);
+    }
+  });
+  if (healthStatusResult.length > 0) {
+    score *= 1 + healthStatusResult.length / 100;
+  }
+
+  //calculating riskscore for good habits
+  googHabits.forEach((goodHabit) => {
+    if (goodHabit.checked) {
+      goodHabitResult.push(goodHabit.value);
+    }
+  });
+  if (goodHabitResult.length > 0) {
+    score *= 1 - (goodHabitResult.length * 5) / 100;
+  }
+
+  //calculating riskscore for bad habits
+  badHabits.forEach((badHabit) => {
+    if (badHabit.checked) {
+      badHabitResult.push(badHabit.value);
+    }
+  });
+  if (badHabitResult.length > 0) {
+    score *= 1 + (badHabitResult.length * 5) / 100;
+  }
+
+  //let #riskscore show score
+  riskscore.textContent = score;
+  if (typeof score === "number") {
+    riskscore.textContent = score.toFixed(2); // No error
+  } else {
+    riskscore.textContent = score; // Handle the case where score is a string
+  }
+};
+
+form.addEventListener("input", riskscoreCal);
